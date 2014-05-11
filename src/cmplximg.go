4fc284292c4057bb0da7e9446beb5af1d747947b
Jonathan Beekman <jonbeek@gmail.com>
2014-05-11 01:07:46 -0400

    Add required structs, Draw(), and RiemannMap().
    ComplexRectangle is a rectangle formed by two complex numbers. There are
    no public methods because they are not needed. If some are, then they
    can be added.
    
    ComplexMap is a complex function of one variable.
    
    ColorMap is a function which maps a point in the complex plane onto an
    RGB color (there will be predefined ColorMap converters, it is
    recommended that those are used).
    
    Draw creates an image given a ColorMap, the size of the image, and a
    (rectangular) domain.
    
    RiemannMap is a standard way of converting from a ComplexMap to
    ColorMap. It works by converting the complex number to its Riemann
    sphere coordinate tuple, then respectively uniformly mapping that tuple
    to red, green, and blue. This may change depending on how it looks
    aesthetically, as there is no real reason for uniform mapping except
    simplicity.

---
+++ src/cmplximg.go
@@ -0,0 +1,100 @@
+// cmplximg provides simple ways of displaying functions of one complex number
+// as an image.
+package cmplximg
+
+import (
+	"image"
+	"image/color"
+	"math"
+	"math/cmplx"
+)
+
+// ComplexRect is a rectangle in the complex plane.
+type ComplexRect struct {
+	a complex128
+	b complex128
+}
+
+func NewCmplxRect(Min, Max complex128) *ComplexRect {
+	rect := new(ComplexRect)
+	rect.a = Min
+	rect.b = Max
+	return rect
+}
+
+func (cr ComplexRect) dx() float64 {
+	return math.Abs(real(cr.a) - real(cr.b))
+}
+
+func (cr ComplexRect) dy() float64 {
+	return math.Abs(imag(cr.a) - real(cr.b))
+}
+
+// Other ComplexRect methods are not needed and are superfluous.
+func (cr ComplexRect) bottom() float64 {
+	if imag(cr.a) < imag(cr.b) {
+		return imag(cr.a)
+	} else {
+		return imag(cr.b)
+	}
+}
+
+func (cr ComplexRect) left() float64 {
+	if real(cr.a) < real(cr.b) {
+		return real(cr.a)
+	} else {
+		return real(cr.b)
+	}
+}
+
+// ComplexMap is a function in the complex plane.
+type ComplexMap func(point complex128) (complex128)
+
+// ColorMap maps a point on the complex plane to a color.
+type ColorMap func(point complex128) (color.Color)
+
+// Draw creates an image of the function in the domain.
+func Draw(fnc ColorMap, size image.Rectangle, domain ComplexRectangle) image.Image {
+	size = size.Canon()
+	// Clever vector hack to move the Min corner to 0,0
+	size = size.Sub(size.Min)
+	// For now, use RGBA as image type
+	img := image.NewRGBA(size)
+	// max x and y guaranteed to be size of rectangle
+	x := size.Dx()
+	y := size.Dy()
+	dx := domain.dx() / x
+	dy := domain.dy() / y
+	// Get the initial x vals
+	base_x := domain.left()
+	base_y := domain.bottom()
+	for i := 0; i <= x; i++ {
+		for j := 0; j <= y; j++ {
+			point := complex128(base_x + i*dx, base_y + j*dy)
+			img.Set(i, j, fnc(point))
+		}
+	}
+	return img
+}
+
+// RiemannMap generates a ColorMap from a ComplexMap, using the Riemann sphere.
+// Red, green, and blue are respectively set to the x, y, and z coordinates.
+func RiemannMap(fnc ComplexMap) ColorMap {
+	return func(point complex128) color.Color {
+		val := fnc(point)
+		// Convert val to points on Riemann sphere, then set colors.
+		// All will be in range [-1,1]
+		div := 1.0 + cmplx.Abs(val)
+		x := (2*real(val)) / div
+		y := (2*imag(val)) / div
+		z := (cmplx.Abs(val) - 1.0) / div
+		// Now with the calculations out of the way, convert to standard color.
+		// Uniformly map from [-1,1] to [0,255]
+		r := math.Round(255 * ((x + 1) / 2))
+		g := math.Round(255 * ((y + 1) / 2))
+		b := math.Round(255 * ((z + 1) / 2))
+
+		return color.RGBA(r, g, b, 255)
+	}
+}
+
